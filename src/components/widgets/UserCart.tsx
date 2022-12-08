3e271908c45fce62b1ea942869ede686a5ad6307
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-08 17:09:29 -0500

    Updated component

--- src/components/widgets/UserCart.tsx
+++ src/components/widgets/UserCart.tsx
@@ -1,16 +1,7 @@
 import { useGetCartQuery } from "../../state/api-cart";
-import { useEffect } from "react";
-import { useAppDispatch } from "../../state/hooks";
-import { changeLocation } from "../../state/location.slice";
-import { useLocation } from "react-router-dom";
 
 function UserCart() {
   const { data } = useGetCartQuery({});
-  const dispatch = useAppDispatch();
-  const { pathname } = useLocation();
-  useEffect(() => {
-    dispatch(changeLocation(pathname));
-  });
   return null;
 }
 
