d6c2d7760bff59253e75cffd1f90888da95b1abc
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-08 07:54:27 -0500

    Updated the store with the new slice
    -- And also saved a copy of the store in the local storage, mainly for
       debugging purposes

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,14 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import api, { API_STORE_KEY, reducer as apiReducer } from "./api";
 import booksReducer, { BOOKS_STORE_NS } from "./books.slice";
+import sessionReducer, { SESSION_STORE_NS } from "./session.slice";
 
+const STORAGE_KEY = "book-store";
 export const store = configureStore({
   reducer: {
     [API_STORE_KEY]: apiReducer,
     [BOOKS_STORE_NS]: booksReducer,
+    [SESSION_STORE_NS]: sessionReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
+});
+
+store.subscribe(() => {
+  const stateToStore = JSON.parse(JSON.stringify(store.getState()));
+  process.env.NODE_ENV === "production" && delete stateToStore[API_STORE_KEY];
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(stateToStore));
 });
 
 export type AppDispatch = typeof store.dispatch;
