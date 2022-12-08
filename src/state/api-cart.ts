a426157ce4aa39746a9befec84832c3368c6d95a
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-08 12:43:50 -0500

    Handled the attempt to call a secured endpoint without being authenticated

--- src/state/api-cart.ts
+++ src/state/api-cart.ts
@@ -1,24 +1,46 @@
-import { createApi } from "@reduxjs/toolkit/dist/query/react";
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/dist/query/react";
 import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 import { RootState } from "./store";
-import { SESSION_STORE_KEY } from "./session.slice";
+import { logout, SESSION_STORE_KEY } from "./session.slice";
 
 export const API_CART_STORE_KEY = "api-cart";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://bookstore-api.gyfted.dev/api",
+  mode: "cors",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState)[SESSION_STORE_KEY].token;
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+      headers.set("Accept", "application/json");
+    }
+    return headers;
+  },
+});
+const baseQueryWithLogout: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    api.dispatch(logout());
+    return Promise.reject("Unauthorized");
+  }
+  return result;
+};
 const apiCart = createApi({
   reducerPath: API_CART_STORE_KEY,
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://bookstore-api.gyfted.dev/api/cart",
-    prepareHeaders: (headers, { getState }) => {
-      headers.set(
-        "Authorization",
-        `Bearer ${(getState() as RootState)[SESSION_STORE_KEY].token}`
-      );
-    },
-  }),
+  baseQuery: baseQueryWithLogout,
   endpoints: (builder) => ({
     getCart: builder.query({
       query: () => ({
-        url: "/",
+        url: "/cart",
       }),
     }),
   }),
