52126d1162435bd718fcf7e606d01ac688a8c416
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-08 07:55:15 -0500

    Init the keycloak services when the app starts

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,27 @@ import { store } from "./state/store";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import keycloakService from "./services/keycloak.service";
+import { login } from "./state/session.slice";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
+
+keycloakService
+  .init({
+    onLoad: "login-required",
+    silentCheckSsoRedirectUri: window.location.origin + "/",
+  })
+  .then((authenticated) => {
+    if (authenticated) {
+      store.dispatch(
+        login({
+          token: `${keycloakService.token}`,
+          refreshToken: `${keycloakService.refreshToken}`,
+        })
+      );
+    }
+  });
 
 root.render(
   <React.StrictMode>
