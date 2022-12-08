0fe3aece54f1d97aa050e4ed59fa71131ddcae62
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-08 13:40:01 -0500

    Update the current path when navigate to user cart

--- src/components/widgets/UserCart.tsx
+++ src/components/widgets/UserCart.tsx
@@ -1,8 +1,16 @@
 import { useGetCartQuery } from "../../state/api-cart";
+import { useEffect } from "react";
+import { useAppDispatch } from "../../state/hooks";
+import { changeLocation } from "../../state/location.slice";
+import { useLocation } from "react-router-dom";
 
 function UserCart() {
   const { data } = useGetCartQuery({});
-  console.log(data);
+  const dispatch = useAppDispatch();
+  const { pathname } = useLocation();
+  useEffect(() => {
+    dispatch(changeLocation(pathname));
+  });
   return null;
 }
 
