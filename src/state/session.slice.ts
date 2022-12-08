6504551ac8c67ac2967656f21bbbb507c2a7f8f9
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-08 12:44:28 -0500

    Slice to store session information

--- src/state/session.slice.ts
+++ src/state/session.slice.ts
@@ -1,14 +1,17 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
 
 type SessionState = {
   authenticated: boolean;
   token: string;
   refreshToken: string;
+  userId: string;
 };
 const initialState: SessionState = {
   authenticated: false,
   token: "",
   refreshToken: "",
+  userId: "",
 };
 export const SESSION_STORE_KEY = "session";
 const sessionSlice = createSlice({
@@ -17,14 +20,27 @@ const sessionSlice = createSlice({
   reducers: {
     login: (
       state: SessionState,
-      { payload }: PayloadAction<{ token: string; refreshToken: string }>
+      {
+        payload,
+      }: PayloadAction<{
+        token: string;
+        refreshToken: string;
+        userId: string;
+      }>
     ) => {
       state.authenticated = true;
       state.token = payload.token;
       state.refreshToken = payload.refreshToken;
+      state.userId = payload.userId;
     },
+    logout: () => initialState,
   },
 });
 
-export const { login } = sessionSlice.actions;
+export const selectAuthenticated = (state: RootState) =>
+  state[SESSION_STORE_KEY].authenticated;
+export const selectUserId = (state: RootState) =>
+  state[SESSION_STORE_KEY].userId;
+
+export const { login, logout } = sessionSlice.actions;
 export default sessionSlice.reducer;
