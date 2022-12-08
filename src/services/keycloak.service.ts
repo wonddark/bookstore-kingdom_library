f62eb979b7d033b9a3af7146d030f248a3cfa6d7
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-08 12:47:00 -0500

    Extended the configuration for the keycloak service

--- src/services/keycloak.service.ts
+++ src/services/keycloak.service.ts
@@ -1,9 +1,46 @@
 import keycloak from "keycloak-js";
+import { store } from "../state/store";
+import { login, logout } from "../state/session.slice";
+import { LOCATION_STORE_KEY } from "../state/location.slice";
 
 const keycloakService = new keycloak({
   url: "https://auth.gyfted.io/",
   realm: "certification",
   clientId: "bookstore-client",
 });
+
+export const initKeycloak = () => {
+  keycloakService.init({
+    onLoad: "check-sso",
+    redirectUri:
+      window.location.origin +
+      (store.getState()[LOCATION_STORE_KEY].location || "/"),
+    checkLoginIframe: false,
+  });
+};
+
+export const loginKeycloak = () => {
+  keycloakService.login();
+};
+
+export const logoutKeycloak = () => {
+  keycloakService.logout({ redirectUri: window.location.origin + "/" });
+};
+
+keycloakService.onAuthSuccess = () => {
+  store.dispatch(
+    login({
+      token: `${keycloakService.token}`,
+      refreshToken: `${keycloakService.refreshToken}`,
+      userId: `${keycloakService.subject}`,
+    })
+  );
+};
+
+keycloakService.onReady = (authenticated) => {
+  if (!authenticated) {
+    store.dispatch(logout());
+  }
+};
 
 export default keycloakService;
