33eedead97ec8005a8a9dc3f4bc5d6ca62d16549
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-11 15:22:51 -0500

    Component for rendering the "add to cart" widget

--- src/components/widgets/AddToCartBtn.tsx
+++ src/components/widgets/AddToCartBtn.tsx
@@ -1,8 +1,37 @@
-function AddToCartBtn({ bookId }: { bookId: string }) {
+import useAddToCart from "../../hooks/add-to-cart.hook";
+
+function AddToCartBtn({ book }: { book: any }) {
+  const { submit, riseQuantity, lowQuantity, updateQuantity, quantity } =
+    useAddToCart({
+      book,
+    });
   return (
-    <button className="btn btn-primary">
-      <i className="bi bi-cart-plus me-2" /> Poner en la cesta
-    </button>
+    <div className="w-75 mx-auto">
+      <div className="input-group">
+        <button
+          className="btn btn-light input-group-text"
+          onClick={riseQuantity}
+        >
+          <i className="bi bi-plus" />1
+        </button>
+        <input
+          type="number"
+          className="form-control"
+          value={quantity}
+          onChange={updateQuantity}
+        />
+        <button
+          className="btn btn-light input-group-text"
+          onClick={lowQuantity}
+          disabled={quantity === 1}
+        >
+          <i className="bi bi-dash" />1
+        </button>
+      </div>
+      <button className="btn btn-primary mt-1" onClick={submit}>
+        <i className="bi bi-cart-plus" /> Poner en la cesta
+      </button>
+    </div>
   );
 }
 
