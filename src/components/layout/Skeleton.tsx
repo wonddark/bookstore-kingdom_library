bab0779a12c9ed8b5d7fd7c01f83aae993195a87
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-12 18:08:33 -0500

    Rendered the toast container

--- src/components/layout/Skeleton.tsx
+++ src/components/layout/Skeleton.tsx
@@ -1,11 +1,23 @@
 import { ReactElement } from "react";
 import Navbar from "./Navbar";
+import { ToastContainer } from "react-toastify";
 
 function Skeleton({ children }: { children: ReactElement }) {
   return (
     <div className="container-fluid p-0">
       <Navbar />
       <>{children}</>
+      <ToastContainer
+        newestOnTop={true}
+        draggable={false}
+        position="bottom-center"
+        autoClose={2500}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnFocusLoss={false}
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 }
