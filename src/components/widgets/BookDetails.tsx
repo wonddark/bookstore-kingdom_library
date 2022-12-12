9e5c01fc90d907ae234143204257f01231795b61
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-12 14:49:44 -0500

    Updated the layout for book detail

--- src/components/widgets/BookDetails.tsx
+++ src/components/widgets/BookDetails.tsx
@@ -22,26 +22,34 @@ function BookDetails() {
         <Row>
           <Col xs={12} md={3} className="text-center mb-4">
             <img src={data.image} alt="book-portrait" className="img-fluid" />
-            <span className="d-block w-100 fs-5">ISBN: {data.isbn13}</span>
+            <span className="d-block w-100 fs-6">ISBN: {data.isbn13}</span>
+            <span className="d-block w-100 fs-6">{data.publisher}</span>
+          </Col>
+          <Col>
+            <h1 className="mb-0 display-1 fs-2">{he.decode(data.title)}</h1>
+            <div className="mb-2">
+              <small className="fw-bolder bg-primary text-light py-1 px-3 mb-2">
+                {data.price}
+              </small>
+              <small className="ms-3 fw-bold text-primary">
+                <i className="bi bi-star-half me-1" />
+                {data.rating}/5
+              </small>
+              <small className="ms-3 text-muted">
+                <i className="bi bi-calendar-event me-1" />
+                {data.year}
+              </small>
+            </div>
+            <h4 className="display-1 fs-5 fst-italic">
+              {he.decode(data.subtitle)}
+            </h4>
+            <h2 className="fs-5 mb-4">{data.authors}</h2>
+            <p className="lead">{he.decode(data.desc)}</p>
             {isAuthenticated ? (
               <div className="mt-2">
                 <AddToCartBtn book={data} />
               </div>
             ) : null}
-          </Col>
-          <Col>
-            <h1>{he.decode(data.title)}</h1>
-            <span className="fs-5 fw-bolder">{data.price}</span>
-            <h3>{he.decode(data.subtitle)}</h3>
-            <h2 className="h4 my-3">{data.authors}</h2>
-            <p className="lead">{he.decode(data.desc)}</p>
-            <span className="fs-5 fw-bolder">Rating: {data.rating} / 5</span>
-            <div className="mt-3 fs-5">
-              <strong>Year:</strong> {data.year}
-            </div>
-            <div className="fs-5">
-              <strong>Publisher:</strong> {data.publisher}
-            </div>
           </Col>
         </Row>
       ) : (
