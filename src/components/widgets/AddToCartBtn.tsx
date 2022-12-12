c0a64494ea8d858028c6aa30a2f235bb3763c9b9
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-12 14:48:23 -0500

    Fixed the responsive display of "add to cart" button

--- src/components/widgets/AddToCartBtn.tsx
+++ src/components/widgets/AddToCartBtn.tsx
@@ -6,31 +6,39 @@ function AddToCartBtn({ book }: { book: any }) {
       book,
     });
   return (
-    <div className="w-75 mx-auto">
-      <div className="input-group">
-        <button
-          className="btn btn-light input-group-text"
-          onClick={riseQuantity}
-        >
-          <i className="bi bi-plus" />1
-        </button>
-        <input
-          type="number"
-          className="form-control"
-          value={quantity}
-          onChange={updateQuantity}
-        />
-        <button
-          className="btn btn-light input-group-text"
-          onClick={lowQuantity}
-          disabled={quantity === 1}
-        >
-          <i className="bi bi-dash" />1
+    <div className="row justify-content-start align-items-center">
+      <div
+        className="col-12 col-sm-auto mb-1"
+        style={{ maxWidth: "fit-content" }}
+      >
+        <div className="input-group">
+          <button
+            className="btn btn-light input-group-text"
+            onClick={riseQuantity}
+          >
+            <i className="bi bi-plus" />1
+          </button>
+          <input
+            type="number"
+            className="form-control"
+            value={quantity}
+            onChange={updateQuantity}
+            style={{ minWidth: "55px", maxWidth: "55px", textAlign: "right" }}
+          />
+          <button
+            className="btn btn-light input-group-text"
+            onClick={lowQuantity}
+            disabled={quantity === 1}
+          >
+            <i className="bi bi-dash" />1
+          </button>
+        </div>
+      </div>
+      <div className="col mb-1">
+        <button className="btn btn-primary text-nowrap" onClick={submit}>
+          <i className="bi bi-cart-plus" /> Poner en la cesta
         </button>
       </div>
-      <button className="btn btn-primary mt-1" onClick={submit}>
-        <i className="bi bi-cart-plus" /> Poner en la cesta
-      </button>
     </div>
   );
 }
