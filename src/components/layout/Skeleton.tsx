945f4f614f8cbfd87186a23fe1cd33b98e098ebe
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-12 16:03:07 -0500

    Refactored the component to not use external library for rendering the navbar

--- src/components/layout/Skeleton.tsx
+++ src/components/layout/Skeleton.tsx
@@ -1,67 +1,71 @@
-import { ReactElement, useState } from "react";
-import {
-  Collapse,
-  Container,
-  Nav,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  NavItem,
-  NavLink,
-} from "reactstrap";
+import { ReactElement } from "react";
 import { useAppSelector } from "../../state/hooks";
 import { selectAuthenticated, selectUserId } from "../../state/session.slice";
+import { Link } from "react-router-dom";
 
 function Skeleton({ children }: { children: ReactElement }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleIsOpen = () => {
-    setIsOpen((prevState) => !prevState);
-  };
   const state = useAppSelector((state) => state);
   const authenticated = selectAuthenticated(state);
   const userId = selectUserId(state);
   return (
-    <Container fluid className="p-0">
+    <div className="container-fluid p-0">
       <header className="sticky-top">
-        <Navbar expand="md" dark color="navbar-dark" container="xl">
-          <NavbarBrand href="/">Kingdom Library</NavbarBrand>
-          <NavbarToggler onClick={toggleIsOpen} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav navbar className="mt-3 mt-md-0 ms-auto">
-              <NavItem>
-                <NavLink href="/books" className="link-success">
-                  Latest
-                </NavLink>
-              </NavItem>
-              {authenticated ? (
-                <NavItem>
-                  <NavLink
-                    href={`/users/${userId}/cart`}
-                    className="link-success"
-                  >
-                    Cart
-                  </NavLink>
-                </NavItem>
-              ) : null}
-              {authenticated ? (
-                <NavItem>
-                  <NavLink href="/logout" className="link-success">
-                    Close session
-                  </NavLink>
-                </NavItem>
-              ) : (
-                <NavItem>
-                  <NavLink href="/authenticate" className="link-success">
-                    Authenticate
-                  </NavLink>
-                </NavItem>
-              )}
-            </Nav>
-          </Collapse>
-        </Navbar>
+        <div className="navbar navbar-expand-md navbar-dark bg-navbar-dark">
+          <div className="container-xl">
+            <Link className="navbar-brand" to="/">
+              Kingdom Library
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mt-3 mt-md-0 ms-auto">
+                <li className="nav-item">
+                  <a href="/books" className="nav-link link-success">
+                    Latest
+                  </a>
+                </li>
+                {authenticated ? (
+                  <li className="nav-item">
+                    <a
+                      href={`/users/${userId}/cart`}
+                      className="nav-link link-success"
+                    >
+                      Cart
+                    </a>
+                  </li>
+                ) : null}
+                {authenticated ? (
+                  <li className="nav-item">
+                    <a href="/logout" className="nav-link link-success">
+                      Close session
+                    </a>
+                  </li>
+                ) : (
+                  <li className="nav-item">
+                    <a href="/authenticate" className="nav-link link-success">
+                      Authenticate
+                    </a>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </div>
+        </div>
       </header>
       <>{children}</>
-    </Container>
+    </div>
   );
 }
 
