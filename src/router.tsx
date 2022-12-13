4957b1ce32a737719517f38e45ca2a791072d2e2
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-12 20:48:09 -0500

    Added a security component to forbid users from visiting other's cart/checkout pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,9 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Link,
   Route,
+  useParams,
 } from "react-router-dom";
 import Home from "./components/pages/Home";
 import BooksHome from "./components/widgets/BooksHome";
@@ -11,6 +13,38 @@ import UserCart from "./components/widgets/UserCart";
 import Authentication from "./components/widgets/Authentication";
 import LogoutControl from "./components/widgets/LogoutControl";
 import Checkout from "./components/widgets/Checkout";
+import { ReactElement } from "react";
+import { useAppSelector } from "./state/hooks";
+import { SESSION_STORE_KEY } from "./state/session.slice";
+
+function CheckAccess({ children }: { children: ReactElement }) {
+  const { userId } = useParams();
+  const storedId = useAppSelector((state) => state[SESSION_STORE_KEY].userId);
+  return (
+    <>
+      {storedId === userId ? (
+        children
+      ) : (
+        <div className="container my-3 py-2 text-center">
+          <p className="display-5 text-danger">
+            You are not allowed to be here
+          </p>
+          <div className="row justify-content-center">
+            <div className="col-12 col-lg-8">
+              <p className="lead mt-3">
+                You seems to be accidentally out of place. Please{" "}
+                <Link to="/">go back home</Link> and keep diving into our great
+                collection of technical books. If this error occurs to you once
+                again be kind and report it to us, we'll do our best to solve it
+                as soon as possible
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -59,7 +93,9 @@ const router = createBrowserRouter(
         path="users/:userId/cart"
         element={
           <Skeleton>
-            <UserCart />
+            <CheckAccess>
+              <UserCart />
+            </CheckAccess>
           </Skeleton>
         }
       />
@@ -67,7 +103,9 @@ const router = createBrowserRouter(
         path="checkout/:userId"
         element={
           <Skeleton>
-            <Checkout />
+            <CheckAccess>
+              <Checkout />
+            </CheckAccess>
           </Skeleton>
         }
       />
