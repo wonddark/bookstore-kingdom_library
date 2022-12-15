df38c664bfbaae8e55da4aa9ab52601657afca68
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-14 20:05:43 -0500

    Extracted the hook for book's data

--- src/components/pages/BooksHome.tsx
+++ src/components/pages/BooksHome.tsx
@@ -1,39 +1,10 @@
-import { useGetBooksQuery, useSearchBooksQuery } from "../../state/api";
 import BookItem from "../widgets/BookItem";
 import SearchForm from "../widgets/SearchForm";
-import { useEffect, useState } from "react";
 import Pagination from "../widgets/Pagination";
-import { useSearchParams } from "react-router-dom";
+import useBookData from "../../hooks/books-data.hook";
 
 function BooksHome() {
-  const [data, setData] = useState([]);
-  const [shouldPaginate, setShouldPaginate] = useState(false);
-  const [readSearchParams] = useSearchParams();
-  const query = readSearchParams.get("query") || "";
-  const page = Number(readSearchParams.get("page")) || 1;
-  const { data: newBooks, isLoading: loadingNewBooks } = useGetBooksQuery(
-    {},
-    { skip: Boolean(query) }
-  );
-  const { data: searchResults, isLoading: loadingSearchResults } =
-    useSearchBooksQuery({ query, page }, { skip: query === null });
-  const loadData = () => {
-    if (query) {
-      if (!loadingSearchResults && searchResults) {
-        setData(searchResults.books);
-        setShouldPaginate(searchResults.total > searchResults.books.length - 1);
-      }
-    } else if (!loadingNewBooks) {
-      newBooks && setData(newBooks.books);
-    }
-  };
-  useEffect(loadData, [
-    loadingNewBooks,
-    loadingSearchResults,
-    newBooks,
-    searchResults,
-    query,
-  ]);
+  const { data, shouldPaginate } = useBookData();
   return (
     <div className="container py-2">
       <div className="row mt-4">
---
+++ src/hooks/books-data.hook.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useGetBooksQuery, useSearchBooksQuery } from "../state/api";
+
+function useBookData() {
+  const [data, setData] = useState([]);
+  const [shouldPaginate, setShouldPaginate] = useState(false);
+  const [readSearchParams] = useSearchParams();
+  const query = readSearchParams.get("query") || "";
+  const page = Number(readSearchParams.get("page")) || 1;
+  const { data: newBooks, isLoading: loadingNewBooks } = useGetBooksQuery(
+    {},
+    { skip: Boolean(query) }
+  );
+  const { data: searchResults, isLoading: loadingSearchResults } =
+    useSearchBooksQuery({ query, page }, { skip: query === null });
+  const loadData = () => {
+    if (query) {
+      if (!loadingSearchResults && searchResults) {
+        setData(searchResults.books);
+        setShouldPaginate(searchResults.total > searchResults.books.length - 1);
+      }
+    } else if (!loadingNewBooks) {
+      newBooks && setData(newBooks.books);
+    }
+  };
+  useEffect(loadData, [
+    loadingNewBooks,
+    loadingSearchResults,
+    newBooks,
+    searchResults,
+    query,
+  ]);
+
+  return { data, shouldPaginate };
+}
+
+export default useBookData;
